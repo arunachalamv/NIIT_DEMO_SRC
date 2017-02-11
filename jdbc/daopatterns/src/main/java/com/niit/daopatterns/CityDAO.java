79e67cfd829d8447f06315955ed23295ee5f3eab
Seshagiri Sriram <SehsagiriSriram@gmail.com>
2017-02-11 18:30:59 +0530

    Updated with Correct DAO

--- jdbc/daopatterns/src/main/java/com/niit/daopatterns/CityDAO.java
+++ jdbc/daopatterns/src/main/java/com/niit/daopatterns/CityDAO.java
@@ -3,5 +3,6 @@ package com.niit.daopatterns;
 import java.util.ArrayList;
 
 public interface CityDAO {
+	public ArrayList getAllCities(String s);
 	public ArrayList getAllCities(); 
 }
--- jdbc/daopatterns/src/main/java/com/niit/daopatterns/MySQLDAOFactory.java
+++ jdbc/daopatterns/src/main/java/com/niit/daopatterns/MySQLDAOFactory.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
 public class MySQLDAOFactory extends DAOFactory {
@@ -19,9 +20,6 @@ public class MySQLDAOFactory extends DAOFactory {
 			USER = PropertyHelper.get("jdbc_user"); 
 			PASS = PropertyHelper.get("jdbc_password"); 
 	 	}
-	   static Connection conn = null;
-	   //static Statement stmt = null;
-	   //static ResultSet rst = null; 
 	   public static void closeRS(ResultSet rs){
 		   try {
 			if (rs!=null && !rs.isClosed()){
@@ -32,6 +30,23 @@ public class MySQLDAOFactory extends DAOFactory {
 			e.printStackTrace();
 		}
 	   }
+	   public static ResultSet executeStatement(Connection conn, String sql, String... args){
+		   com.mysql.jdbc.PreparedStatement stmt = null; 
+		ResultSet rs = null; 
+		try{ 
+			stmt = (PreparedStatement) conn.prepareStatement(sql);
+			int cntr =1; 
+			for (String args1: args){
+				stmt.setString(cntr++,args1); 
+			}
+			rs = stmt.executeQuery();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		return rs; 
+	   }
+	   
 	   public static ResultSet executeStatement(Connection conn,String sql){
 		   Statement stmt = null; 
 		   ResultSet rs = null; 
@@ -47,21 +62,18 @@ public class MySQLDAOFactory extends DAOFactory {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		   try {
-			if(stmt!=null && !stmt.isClosed()) stmt.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
 		   return rs; 
 	   }
 	   public static Connection getConnection() {
 		   init(); 
 		    // Use DRIVER and DBURL to create a connection
 		    // Recommend connection pool implementation/usage
-		    if (conn!= null) return conn; 
+		    //if (conn!= null) return conn;
+		   Connection conn = null; 
 		      try {
 				conn = (Connection)DriverManager.getConnection(DB_URL,USER,PASS);
+				conn.setAutoCommit(false);
+				conn.setAutoClosePStmtStreams(true);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -69,7 +81,7 @@ public class MySQLDAOFactory extends DAOFactory {
 		      return conn; 
 		  }
 	   
-	   public static void closeConnection() { 
+	   public static void closeConnection(Connection conn) { 
 		   if (conn!=null){ 
 			   try {
 				if(!conn.isClosed()){
