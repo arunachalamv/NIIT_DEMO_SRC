e7d9c46b1efcaa233f5550f3d78fb9547f38bb04
Seshagiri Sriram <SehsagiriSriram@gmail.com>
2017-02-17 13:05:34 +0530

    Updated Parent .gitignore to exclude .project/.classpath/.settings

--- helloworld/src/main/java/com/niit/helloworld/HelloWorld.java
+++ helloworld/src/main/java/com/niit/helloworld/HelloWorld.java
@@ -6,8 +6,6 @@ public class HelloWorld {
 
 	// All java programs begin with this function.. 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// System.out.println is predefined method for ou
 		System.out.println("Hello World");
 	}
 
