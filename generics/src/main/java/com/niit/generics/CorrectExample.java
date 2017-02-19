7bcc5ffdb02242ac55808cd112852042637cf7fe
Seshagiri Sriram <SehsagiriSriram@gmail.com>
2017-02-19 12:56:25 +0530

    Updated with Correct Example

---
+++ generics/src/main/java/com/niit/generics/CorrectExample.java
@@ -0,0 +1,23 @@
+package com.niit.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CorrectExample {
+	public static void main(String ... args){
+		// Create list
+		List <Stamp>stamps = new ArrayList<Stamp>(); 
+		// Add values ... 
+		stamps.add(new Stamp("India",2016,true,200.00));
+		// Now below 2 lines will ***** NOT COMPILE ***
+		//stamps.add(new Coin("USA",2016,true,200.00,"Denver","Obverse Strike"));
+		//stamps.add(new Coin("USA",2014,true,200.00,"Philadelphia","Reverse Strike"));
+		stamps.add(new Stamp("India",2017,false,400.00));
+		// now how do we iterate only over stamps and not coins??
+		for (Stamp c: stamps){
+			System.out.println(c);
+		}
+		
+	}
+}
+
